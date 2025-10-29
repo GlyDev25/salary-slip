3a53b59b0822dfde49d996ab6aa0bd3c762cdf9a
olojoemmanuel <olojoemmanuel25@gmail.com>
2025-10-29 23:11:33 +0100

    initial commit

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
 name = input('What is your name, surname first? ')
 hours = int(input('What is the total number of hours you worked this week? '))
 salary = hours * 250 * 4
-print(f'\Hi! {name}, because you worked {hours} weekly, your earnings this month is {salary}, thank you for your diligence')
+print(f'\Hi! {name}, because you work {hours} weekly, your earnings this month is {salary}, thank you for your diligence')
---
+++ rock-density.py
@@ -0,0 +1,5 @@
+rock_density = print('this is a rock density calculator')
+rock_mass = int(input('enter the mass of the rock _ '))
+rock_volume = int(input('enter the volume of the rock _ '))
+rock_density = rock_mass/rock_volume
+print(f'the density of this rock is {rock_density}') 
